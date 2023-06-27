edc406ac2729e05405bb54907d4d42db67a45c7b
Luana de Paula L Ramos <luanadepaulalr@gmail.com>
2023-06-26 22:22:03 -0300

    seção adicionar filme da pag sobre

--- pagina__sobre/sobreOProjeto.js
+++ pagina__sobre/sobreOProjeto.js
@@ -1,6 +1,18 @@
 
+/*
 
-function incricao() {
-    let email = document.getElementById("emailNews").value;
-    alert("O email: " + email + " está inscrito!")
+    function incricao() {
+        let email = document.getElementById("emailNews").value;
+        alert("O email: " + email + " está inscrito!")
+    }
+
+
+*/
+
+fetch('http://www.omdbapi.com/?i=tt3896198&apikey=934ce73f')
+.then(response => response.json())
+
+let filmes = document.querySelector("input#nomeFilme")
+function enviarFilme(){
+    alert(filmes.value)
 }

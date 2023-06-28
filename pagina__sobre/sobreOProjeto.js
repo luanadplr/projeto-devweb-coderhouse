ece5d7a60725023c2023d7393b184cd490c7f99e
Arthur Botelho <emilioarthur19@gmail.com>
2023-06-27 21:17:21 -0300

    function da API de busca de filme atualizado

--- pagina__sobre/sobreOProjeto.js
+++ pagina__sobre/sobreOProjeto.js
@@ -9,15 +9,32 @@
 
 */
 
-fetch('http://www.omdbapi.com/?i=tt3896198&apikey=f2ea36dc')
-    .then(response => response.json())
-    .then(response => enviarFilme(response))
+function buscarFilme() {
+    let filmes = document.getElementById("nomeFilme").value;
+    let url = 'http://www.omdbapi.com/?t=' + filmes + '&apikey=f2ea36dc'
+    console.log(url)
 
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
 
+            let filmeBuscado = document.getElementById("filmeBuscado")
+
+            filmeBuscado.append(data.Title)
+
+            console.log("Dados da API: ", data.Title)
+
+        })
+
+        .catch(error => {
+
+            console.log("Ocorreu um erro: ", error);
+
+        });
+}
 
 function enviarFilme() {
     let filmes = document.getElementById("nomeFilme").value;
-
     let filmeEnviado = document.getElementById("filmeEnviado")
     alert("O filme: " + filmes + " foi enviado!")
 

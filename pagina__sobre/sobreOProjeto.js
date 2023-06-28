e9655cb2fda74656471bd6107fba5d33d282f988
Luana de Paula L Ramos <luanadepaulalr@gmail.com>
2023-06-27 22:31:09 -0300

    ajustes iniciais na api

--- pagina__sobre/sobreOProjeto.js
+++ pagina__sobre/sobreOProjeto.js
@@ -1,41 +1,33 @@
 
-/*
-
-    function incricao() {
-        let email = document.getElementById("emailNews").value;
-        alert("O email: " + email + " está inscrito!")
-    }
-
-
-*/
 
 function buscarFilme() {
     let filmes = document.getElementById("nomeFilme").value;
-    let url = 'http://www.omdbapi.com/?t=' + filmes + '&apikey=f2ea36dc'
+    let url = `http://www.omdbapi.com/?t=${filmes}&apikey=f2ea36dc`
     console.log(url)
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-
-            let filmeBuscado = document.getElementById("filmeBuscado")
-
-            filmeBuscado.append(data.Title)
-
-            console.log("Dados da API: ", data.Title)
-
+                let filmeBuscado = document.getElementById("filmeBuscado")
+                filmeBuscado.append(` ${data.Title} - Produzido em: ${data.Country}`)
+                console.log("Dados da API: ",data)
         })
 
-        .catch(error => {
+        /*   
+        
+        AJUSTAR O ERROR E O REPETIDO
 
-            console.log("Ocorreu um erro: ", error);
+        catch(e){
+            let mensagemErro = document.querySelector("p#filmeBuscado")
+            mensagemErro.innerHTML = `O Título inserido não está correto.`
+            console.log("Ocorreu um erro: ", error)
+        }
 
-        });
+        */
 }
-
 function enviarFilme() {
     let filmes = document.getElementById("nomeFilme").value;
     let filmeEnviado = document.getElementById("filmeEnviado")
-    alert("O filme: " + filmes + " foi enviado!")
+    alert(`O filme ${filmes} foi enviado com sucesso!`)
 
 }

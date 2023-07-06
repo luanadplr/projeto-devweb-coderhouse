c00110cd9f3d7c9c211299542fa022f713e1aaa1
Luana de Paula L Ramos <luanadepaulalr@gmail.com>
2023-07-06 15:11:12 -0300

    ajustes necessarios

--- script.js
+++ script.js
@@ -44,28 +44,39 @@ function aleatorio(){
     location.href=links[numero]
 }
 
+let rodape = document.querySelector("footer#footer--norte")
+
 function verde(){
     document.body.style.backgroundColor = '#24F27B'
+    rodape.style.backgroundColor = '#24F27B'
 }
 
 function laranja(){
     document.body.style.backgroundColor = 'rgb(255, 172, 84)'
+    rodape.style.backgroundColor = 'rgb(255, 172, 84)'
 }
 
 function amarelo(){
-    document.body.style.backgroundColor = '#E5FE52'
+    document.body.style.backgroundColor = '#D2FF00'
+    rodape.style.backgroundColor = '#D2FF00'
 }
 
 function lilas(){
     document.body.style.backgroundColor = '#FACAFF'
+    rodape.style.backgroundColor = '#FACAFF'
 }
 
 function azul(){
     document.body.style.backgroundColor = '#8CDDFF'
+    rodape.style.backgroundColor = '#8CDDFF'
 }
 
 function branco() {
     document.body.style.backgroundColor = '#FFFFFF'
+}
+
+function cinza(){
+    rodape.style.backgroundColor = "#F2F2F2"
 }
 
 
